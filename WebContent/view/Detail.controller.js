deb84e937dbaa520a81c48af4696ba446887ddf3
dkcasbin <cassio.binkowski@lego.com>
2016-08-23 14:40:17 +0200

    rearrange folders

---
+++ WebContent/view/Detail.controller.js
@@ -0,0 +1,3 @@
+sap.ui.controller("sap.ui5.demo.view.Detail", {
+
+});
---
+++ WebContent/view/Master.controller.js
@@ -0,0 +1,19 @@
+sap.ui.controller("sap.ui5.demo.view.Master", {
+   serviceEndPoint = "http://<host>:<port>/<service_end_point>",
+   onInit: function() {
+   		var modelURL = serviceEndPoint+"/Airlines?$format=json";
+   		var omodel = new sap.ui.model.json.JSONModel(sUrl);
+		this.getView().byId('masterList').setModel(omodel);
+   },   
+   onItemPress: function(oEvent){
+		// To get the selected Airline ID
+		var oItemSelect = oEvent.getParameter("listItem");
+		var Context = oItemSelect.getBindingContext();
+		var carrid = Context.getProperty("Carrid");
+			
+		// Get the Flights from the selected Airline, and set the model to the details page
+		var sUrl = serviceEndPoint+"/Airlines(" + carrid + ")/ToFlights?$format=json";
+		var omodel = new sap.ui.model.json.JSONModel(sUrl);
+		sap.ui.getCore().byId("detailsTable").setModel(omodel);
+   }
+});
