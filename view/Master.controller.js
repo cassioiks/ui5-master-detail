2d76926a5f42937196609dfaa138580433081d4c
dkcasbin <cassio.binkowski@lego.com>
2016-08-23 13:07:18 +0200

    add details page

--- view/Master.controller.js
+++ view/Master.controller.js
@@ -1,6 +1,7 @@
 sap.ui.controller("lego.ui5.demo.view.Master", {
+   serviceEndPoint = "http://<host>:<port>/<service_end_point>",
    onInit: function() {
-   		var modelURL = "http://<host>:<port>/<service_end_point>/Airlines?$format=json";
+   		var modelURL = serviceEndPoint+"/Airlines?$format=json";
    		var omodel = new sap.ui.model.json.JSONModel(sUrl);
 		this.getView().byId('masterList').setModel(omodel);
    },   
@@ -11,8 +12,8 @@ sap.ui.controller("lego.ui5.demo.view.Master", {
 		var carrid = Context.getProperty("Carrid");
 			
 		// Get the Flights from the selected Airline, and set the model to the details page
-		var sUrl = "http://<host>:<port>/<service_end_point>/Airlines(" + carrid + ")/ToFlights?$format=json";
+		var sUrl = serviceEndPoint+"/Airlines(" + carrid + ")/ToFlights?$format=json";
 		var omodel = new sap.ui.model.json.JSONModel(sUrl);
-		sap.ui.getCore().byId("detailList").setModel(omodel);
+		sap.ui.getCore().byId("detailsTable").setModel(omodel);
    }
 });
