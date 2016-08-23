07848be5cc7ec1afa43edb2c4d86c1c42f5e1e33
dkcasbin <cassio.binkowski@lego.com>
2016-08-23 11:32:10 +0200

    initial master detail pa pages

---
+++ view/Master.controller.js
@@ -0,0 +1,18 @@
+sap.ui.controller("lego.ui5.demo.view.Master", {
+   onInit: function() {
+   		var modelURL = "http://<host>:<port>/<service_end_point>/Airlines?$format=json";
+   		var omodel = new sap.ui.model.json.JSONModel(sUrl);
+		this.getView().byId('masterList').setModel(omodel);
+   },   
+   onItemPress: function(oEvent){
+		// To get the selected Airline ID
+		var oItemSelect = oEvent.getParameter("listItem");
+		var Context = oItemSelect.getBindingContext();
+		var carrid = Context.getProperty("Carrid");
+			
+		// Get the Flights from the selected Airline, and set the model to the details page
+		var sUrl = "http://<host>:<port>/<service_end_point>/Airlines(" + carrid + ")/ToFlights?$format=json";
+		var omodel = new sap.ui.model.json.JSONModel(sUrl);
+		sap.ui.getCore().byId("idPrdList").setModel(omodel);
+   }
+});
